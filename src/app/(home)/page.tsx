0dbcd9d3eb92c736e678e4ca9e2759f655d7a0b7
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-11 15:53:53 +0000

    w

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 export default function Page() {
   return (
-    <><div className={"container"}>
+    <>
+    <div className={"container"}>
       <div className={"card-container"}>
         <div className={"card"}>
           <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
@@ -12,23 +13,29 @@ export default function Page() {
             <button>Add to Cart</button>
           </div>
         </div>
+
+        <div className={"card"}>
+          <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
+          <div className={"body"}>
+            <div className={"title"}>Big Bang Seaweed</div>
+          </div>
+          <div className={"footer"}>
+            <input type="number" value={1}></input>
+            <button >Add to Cart</button>
+          </div>
+        </div>
       </div>
     </div>
+  </>
+);
+}
 
 
-          <div className={"card"}>
-            <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
-            <div className={"body"}>
-              <div className={"title"}>Big Bang Seaweed</div>
-            </div>
-            <div className={"footer"}>
-              <input type="number" value={1}></input>
-              <button >Add to Cart</button>
-            </div>
-          </div>
 
-     </>
 
-  );
+
+
+
+
 
 }
