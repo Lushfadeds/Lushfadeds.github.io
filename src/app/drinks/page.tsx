47201b907f9df058bf5ae410a991c06f7c5c13e5
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-11 14:17:17 +0000

    wow

---
+++ src/app/drinks/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1><Drinks></Drinks>/h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <div className={"navigation"}>
           <h1>Welcome to my snack shop!</h1>
+
           <button className="login-button">Sign Up / Log In</button>
+          
           <nav>
             <Link href={"/"}>Home</Link>
             <Link href={"/snacks"}>Snacks</Link>
             <Link href={"/candies"}>Candies</Link>
             <Link href={"/drinks"}>Drinks</Link>
-            <Link href={"/contact"}>Contact u</Link>
+            <Link href={"/contact"}>Contact us</Link>
           </nav>
         </div>
         <main>{children}</main>
