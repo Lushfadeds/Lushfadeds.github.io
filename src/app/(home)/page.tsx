b0d507ce41e5706424ab485826f4f6ca066db0c0
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-11 15:52:12 +0000

    w

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,20 @@
 export default function Page() {
   return (
-    <>
-      <div className={"container"}>
-        <div className={"card-container"}>
-          <div className={"card"}>
-            <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
-            <div className={"body"}>
-              <div className={"title"}>Tom & Jerry Crackers</div>
-            </div>
-            <div className={"footer"}>
-              <input type="number" value={1}></input>
-              <button>Add to Cart</button>
-            </div>
+    <><div className={"container"}>
+      <div className={"card-container"}>
+        <div className={"card"}>
+          <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
+          <div className={"body"}>
+            <div className={"title"}>Tom & Jerry Crackers</div>
           </div>
+          <div className={"footer"}>
+            <input type="number" value={1}></input>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
           <div className={"card"}>
             <img src={"https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"} />
@@ -24,17 +26,9 @@ export default function Page() {
               <button >Add to Cart</button>
             </div>
           </div>
-        </div>
-      </div>
-    </>
+
+     </>
+
   );
-}
-This will display two cards side by side with equal spacing.
-
-
-
-
-
-
 
 }
