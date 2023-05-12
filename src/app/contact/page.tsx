5d69f84c436dd9e7b1c4d86de4a362cdd2e4488f
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-12 16:35:46 +0000

    bruh what

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,10 +12,8 @@ export default function Page() {
     <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram logo" />
     <p>Instagram</p>
   </a>
-  <a href="https://www.instagram.com" target="_blank">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram logo" />
-    <p>Instagram</p>
-  </a>
+
+
 </div>
 </>
  );
