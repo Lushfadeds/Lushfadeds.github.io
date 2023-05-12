528b92417c4d1d2fb99d75bdb28eb89eaddb540d
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-12 15:26:24 +0000

    trying

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
       <h1>Contact</h1>
     </div>
       <div className="contact-section">
-        wow
+      <img src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AInstagram_logo_2016.svg&psig=AOvVaw2tfDXZVX2-oF6xA4g6m0N4&ust=1683991432971000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCOC8pouL8P4CFQAAAAAdAAAAABAE"} />
       </div></>
   );
 }
