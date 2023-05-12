f9b4a5e6c4fe64b4329bd7f45437de52d461f5b6
Lushfadeds <133088511+Lushfadeds@users.noreply.github.com>
2023-05-12 17:02:01 +0000

    LOGOS

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,11 +8,22 @@ export default function Page() {
     <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram logo" />
     <p>Instagram</p>
   </a>
-  <a href="https://www.instagram.com" target="_blank">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram logo" />
-    <p>Instagram</p>
+  <a href="https://www.facebook.com/" target="_blank">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="Instagram logo" />
+    <p>Facebook</p>
   </a>
-
+  <a href="https://web.telegram.org/k/" target="_blank">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png" alt="Instagram logo" />
+    <p>Telegram</p>
+  </a>
+  <a href="https://twitter.com/" target="_blank">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/1245px-Logo_of_Twitter.svg.png" alt="Instagram logo" />
+    <p>twitter</p>
+  </a>
+  <a href="https://www.youtube.com/?themeRefresh=1" target="_blank">
+    <img src="https://www.freepnglogos.com/uploads/youtube-logo-hd-8.png" alt="Instagram logo" />
+    <p>Youtube</p>
+  </a>
 
 </div>
 </>
